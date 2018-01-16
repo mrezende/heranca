063547077aba817bf27a820284edfa54fd9de6d6
cursologicaverao <cursologicaverao@hotmail.com>
2018-01-16 16:25:09 -0200

    removing one secretaria

--- TestaHeranca.php
+++ TestaHeranca.php
@@ -18,15 +18,6 @@ echo "Usuario: {$gerente->usuario}\n";
 echo "Senha: {$gerente->senha}\n";
 
 $secretaria = new Secretaria();
-$secretaria->nome = "Jonas";
-$secretaria->salario = 7000;
-$secretaria->estacaoDeTrabalho = 6241;
-
-echo "Secretaria: {$secretaria->nome}\n";
-echo "Salário: {$secretaria->salario}\n";
-echo "Estação de trabalho: {$secretaria->estacaoDeTrabalho}\n";
-
-$secretaria = new Secretaria();
 $secretaria->nome = "Rafael";
 $secretaria->salario = 9000;
 $secretaria->estacaoDeTrabalho = 7393;
